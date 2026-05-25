013b01c81b3165f6e785ea3a340877f41c87ab33
Mario A <mario-devops@localhost.localdomain>
2026-05-25 17:10:13 +0100

    upgrade to v2.0 with blue-green strategy

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # We use a blue color for Version 1.0
+    # We use lightgreen for Version 2.0
     return """
-    <div style="background-color: #007bff; color: white; padding: 50px; text-align: center; font-family: sans-serif;">
+    <div style="background-color: lightgreen; color: black; padding: 50px; text-align: center; font-family: sans-serif;">
         <h1>Hello! This is my own app running on Argo CD.</h1>
-        <p style="font-size: 24px;">This is the version 1.0 of this python app.</p>
+        <p style="font-size: 24px;">This is the version 2.0 of this python app.</p>
+        <p style="font-size: 18px;">BlueGreen Deployment Strategy - Argo Rollouts</p>
     </div>
     """
 
